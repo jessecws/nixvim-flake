b869a4b6b6d25fb266ec957852c51bed6ba22336
Jesse Herrera <jesse@cephaloworks.com>
2024-01-29 12:15:18 -0600

    tokyo night cursor color
    still need to fixup undodir with var

--- config/core.nix
+++ config/core.nix
@@ -42,5 +42,21 @@
 
       colorcolumn = "80";
     };
+
+  extraConfigLua = ''
+    function ColorMyPencils(color)
+        color = color or "tokyonight-night"
+        vim.cmd.colorscheme(color)
+
+        -- vim.api.nvim_set_hl(0, "Normal", {bg = "none" })
+        -- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
+        vim.cmd.highlight({ "LineNr", "guifg=#456dd6" })
+        vim.cmd.highlight({ "CursorLineNr", "guifg=yellow" })
+
+    end
+
+    ColorMyPencils()
+  '';
+
   };
 }
