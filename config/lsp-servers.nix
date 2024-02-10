00b21128c647e6ffa7a2e40201203fc710ca7e56
Jesse Herrera <jesse@cephaloworks.com>
2024-02-10 17:05:03 -0600

    install rustsupport for lsp

--- config/lsp-servers.nix
+++ config/lsp-servers.nix
@@ -3,51 +3,55 @@
     lsp = {
       enable = true;
       servers = {
-        
-	# nix
-	nixd.enable = true;
+
+        # nix
+        nixd.enable = true;
 
         # ansible
-	ansiblels.enable = true;
+        ansiblels.enable = true;
 
-	# bash
-	bashls.enable = true;
+        # bash
+        bashls.enable = true;
 
-	# docker
-	dockerls.enable = true;
+        # docker
+        dockerls.enable = true;
 
-	# go
-	gopls.enable = true;
+        # go
+        gopls.enable = true;
 
-	# html
-	html.enable = true;
+        # html
+        html.enable = true;
 
-	# markdown
-	marksman.enable = true;
+        # markdown
+        marksman.enable = true;
 
-	#python
-	pyright.enable = true;
+        #python
+        pyright.enable = true;
 
-	#yaml
-	yamlls.enable = true;
+        #yaml
+        yamlls.enable = true;
 
         # javascript / typescript
         tsserver.enable = true;
 
-	# lua
-	lua-ls.enable = true;
+        # lua
+        lua-ls.enable = true;
 
-	# rust
-	rust-analyzer.enable = true;
+        # rust
+        rust-analyzer = {
+          enable = true;
+          installCargo = true;
+          installRustc = true;
+        };
 
-	# terraform
-	terraformls.enable = true;
+        # terraform
+        terraformls.enable = true;
 
-	# graphql
-	graphql.enable = true;
+        # graphql
+        graphql.enable = true;
 
-	# 
+        #
       };
-      };
-};
+    };
+  };
 }
