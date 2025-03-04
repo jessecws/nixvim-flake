6e0a04bcdadbfc5a0ab53269c6336c669c0cbb47
jesse <jesse@cephaloworks.com>
2025-03-03 21:48:33 -0600

    update rendermarkdown for avante

--- config/avante.nix
+++ config/avante.nix
@@ -3,57 +3,66 @@
   extraConfigLua = ''
     require('img-clip').setup({ })
   '';
-  plugins.avante = {
-    enable = true;
-    settings = {
-      auto_suggestions_provider = "claude";
-      tokenizer = "tiktoken";
-      system_prompt = ''
-        You are an excellent programming expert.
-      '';
-      claude = {
-        endpoint = "https://api.anthropic.com";
-        max_tokens = 8000;
-        model = "claude-3-7-sonnet-20250219";
-        temperature = 0;
+  plugins = {
+    render-markdown = {
+      enable = true;
+      settings = {
+        file_types = [ "markdown" "Avante" ];
+        ft = [ "markdown" "Avante" ];
       };
-      diff = {
-        autojump = true;
-        debug = false;
-        list_opener = "copen";
-      };
-      highlights = {
+    };
+    avante = {
+      enable = true;
+      settings = {
+        auto_suggestions_provider = "claude";
+        tokenizer = "tiktoken";
+        system_prompt = ''
+          You are an excellent programming expert.
+        '';
+        claude = {
+          endpoint = "https://api.anthropic.com";
+          max_tokens = 8000;
+          model = "claude-3-7-sonnet-20250219";
+          temperature = 0;
+        };
         diff = {
-          current = "DiffText";
-          incoming = "DiffAdd";
+          autojump = true;
+          debug = false;
+          list_opener = "copen";
         };
-      };
-      behaviour = {
-        auto_suggestions = false;
-        auto_set_highlight_group = true;
-        auto_set_keymaps = true;
-        auto_apply_diff_after_generation = false;
-        support_paste_from_clipboard = false;
-      };
-      hints = { enabled = true; };
-      mappings = {
-        diff = {
-          both = "cb";
-          next = "]x";
-          none = "c0";
-          ours = "co";
-          prev = "[x";
-          theirs = "ct";
+        highlights = {
+          diff = {
+            current = "DiffText";
+            incoming = "DiffAdd";
+          };
         };
-      };
-      provider = "claude";
-      windows = {
-        sidebar_header = {
-          align = "center";
-          rounded = true;
+        behaviour = {
+          auto_suggestions = false;
+          auto_set_highlight_group = true;
+          auto_set_keymaps = true;
+          auto_apply_diff_after_generation = false;
+          support_paste_from_clipboard = false;
         };
-        width = 40;
-        wrap = true;
+        hints = { enabled = true; };
+        mappings = {
+          diff = {
+            both = "cb";
+            next = "]x";
+            none = "c0";
+            ours = "co";
+            prev = "[x";
+            theirs = "ct";
+          };
+        };
+        provider = "claude";
+        windows = {
+          sidebar_header = {
+            align = "center";
+            rounded = true;
+          };
+          width = 40;
+          wrap = true;
+        };
       };
     };
   };
